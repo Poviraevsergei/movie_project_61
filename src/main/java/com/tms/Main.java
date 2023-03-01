3bd4515744270dab468a136fbfc2438d10945e56
Poviraevsergei <pacmanmozg1441>
2023-03-01 21:26:51 +0300

    Spring Boot + actuator

--- src/main/java/com/tms/Main.java
+++ src/main/java/com/tms/Main.java
@@ -1,15 +1,12 @@
 package com.tms;
 
-import com.tms.config.SpringConfig;
-import com.tms.domain.Movie;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        SpringApplication.run(Main.class, args);
     }
 }
-
-//TODO: check + url send
--- src/main/java/com/tms/exceptions/ExceptionResolver.java
+++ src/main/java/com/tms/exceptions/ExceptionResolver.java
@@ -3,12 +3,15 @@ package com.tms.exceptions;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@CacheEvict
 public class ExceptionResolver {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
